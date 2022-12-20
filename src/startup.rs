76fcb88cf0574626d46f98c91c0c34b95ca902ea
jclmnop <jcheshire@protonmail.com>
2022-12-20 19:49:29 +0000

    use connection pool (PgPool) in app_data

--- src/startup.rs
+++ src/startup.rs
@@ -1,21 +1,23 @@
 use crate::endpoint::Endpoint::{ExamplePost, HealthCheck};
 use actix_web::dev::{Server, ServiceFactory, ServiceRequest};
 use actix_web::{App, Error, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 /// Run the server using the provided TCP Listener
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| build_app(App::new()))
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(move || build_app(App::new(), &db_pool))
         .listen(listener)?
         .run();
     Ok(server)
 }
 
 /// Build the actix-web application
-fn build_app<T>(app: App<T>) -> App<T>
+fn build_app<T>(app: App<T>, db_pool: &PgPool) -> App<T>
 where
     T: ServiceFactory<ServiceRequest, Config = (), Error = Error, InitError = ()>,
 {
     app.route(HealthCheck.get_path(), HealthCheck.get_handler())
         .route(ExamplePost.get_path(), ExamplePost.get_handler())
+        .app_data(db_pool.clone())
 }
