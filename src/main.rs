c802bd7538a5b59163736c0b4715eadd969aa8fa
jclmnop <jcheshire@protonmail.com>
2022-12-28 16:43:16 +0000

    set up tracing for async functions and add logging middleware to actix-web app

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,15 @@ use actix_web_template::configuration::Settings;
 use actix_web_template::startup::run;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use actix_web_template::telemetry::{get_subscriber, init_subscriber};
+
+const APP_NAME: &str = "example-app";
+const DEFAULT_LOG_LEVEL: &str = "info";
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber(APP_NAME.into(), DEFAULT_LOG_LEVEL.into());
+    init_subscriber(subscriber);
     let configuration = Settings::get_config().expect("Failed to load config");
     let db_pool = PgPool::connect(&configuration.database.connection_string())
         .await
--- src/startup.rs
+++ src/startup.rs
@@ -1,26 +1,24 @@
 use crate::endpoint::{example_get, example_post, health_check};
-use actix_web::dev::{Server, ServiceFactory, ServiceRequest};
-use actix_web::{web, App, Error, HttpServer};
+use actix_web::dev::Server;
+use actix_web::middleware::Logger;
+use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 /// Run the server using the provided TCP Listener
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(move || build_app(App::new(), db_pool.clone()))
-        .listen(listener)?
-        .run();
-    Ok(server)
-}
-
-//TODO: replace with config
-/// Build the actix-web application
-fn build_app<T>(app: App<T>, db_pool: PgPool) -> App<T>
-where
-    T: ServiceFactory<ServiceRequest, Config = (), Error = Error, InitError = ()>,
-{
     let connection_pool = web::Data::new(db_pool);
-    app.service(health_check)
-        .service(example_get)
-        .service(example_post)
-        .app_data(connection_pool)
+    // Build the app
+    let server = HttpServer::new(move || {
+        //TODO: add service in config fn instead
+        App::new()
+            .wrap(Logger::default())
+            .service(health_check)
+            .service(example_get)
+            .service(example_post)
+            .app_data(connection_pool.clone())
+    })
+    .listen(listener)?
+    .run();
+    Ok(server)
 }
