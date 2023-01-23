c58a30ed14215b9b491ced5d652de17cf06879ab
jclmnop <jcheshire@protonmail.com>
2023-01-23 13:24:37 +0000

    add telemetry macro()! to create and enter span guard

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -23,3 +23,44 @@ pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
     set_global_default(subscriber).expect("Failed to set subscriber");
     tracing::info!("Subscriber set.");
 }
+
+/// Creates a unique ID for the current request, creates the `info_span` and
+/// then enters the span to create the guard.
+///
+/// First arg is a string literal which is used as a "name" for the span.
+/// The est of the arguments are fields to be displayed within the trace. `%`
+/// can be used to automatically name the field. For example, `%field`, if field
+/// had a value of 69, would be displayed as `field: 69`.
+/// ```rust
+/// # use actix_web::{web, HttpResponse};
+/// # use sqlx::PgPool;
+/// use tracing::Instrument;
+/// # use actix_web_template::init_request_trace;
+/// pub async fn example_get(
+///     email: web::Path<String>,
+///     pool: web::Data<PgPool>,
+/// ) -> HttpResponse {
+///     init_request_trace!("Processing new GET request", %email);
+///     todo!()
+/// }
+#[macro_export]
+macro_rules! init_request_trace {
+    ($name:literal, $($field:tt)*) => {
+        let request_id = ::uuid::Uuid::new_v4();
+        let request_span = ::tracing::info_span!(
+            $name,
+            %request_id,
+            $($field)*
+        );
+        let _request_span_guard = request_span.enter();
+    };
+    ($name:literal) => {
+        let request_id = ::uuid::Uuid::new_v4();
+        let request_span = ::tracing::info_span!(
+            $name,
+            %request_id,
+        );
+        let _request_span_guard = request_span.enter();
+    };
+}
+pub(crate) use init_request_trace;
